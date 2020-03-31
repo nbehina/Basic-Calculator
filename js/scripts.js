0e85c0d815b1e2d7f868aa6aa04be5a6710572d7
Your Namenbehina <nbehina@gmail.com>
2020-03-30 18:38:30 -0600

    Basic Calculator

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var addition = (a,b)=>a+b; 
+console.log(addition(4,3));
+
+var subtraction = (a,b)=>a-b; 
+console.log(subtraction(4,3));
+
+var multiply = (a,b)=>a*b; 
+console.log(multiply(4,3));
+
+var divition = (a,b)=>a/b; 
+console.log(divition(4,3));
+
+
